15389fcf75c0b435f96ccabf93a8088ad9e8d221
Solane <simonelostimolo@gmail.com>
2025-10-21 06:57:55 +0200

    chore(config): add path alias for src directory

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/PokeMMO-Compendium/",
   build: {
     outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
 });
